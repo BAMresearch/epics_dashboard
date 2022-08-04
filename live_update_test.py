24ba2689e3288cd7e175f4c9e1f453aa586d3725
jfrontze <julius.frontzek@bam.de>
2022-08-04 15:09:27 +0200

    Somewhat working prototype; Missing: functionality to set number of data points; bugs: Somethis this exception is thrown: 'RuntimeError: dictionary changed size during iteration' --> have to investigate, other bug: Time starts at zero, not actual time

--- live_update_test.py
+++ live_update_test.py
@@ -3,6 +3,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import time
+
+from sympy import Idx
 import dummy_data_thread
 import threading
 from CaprotoReaders.caproto_live_reader import CALiveReader
@@ -12,23 +14,22 @@ from typing import List
 def get_pvs_from_txt(filename: str) -> List[str]:
     with open(filename) as file:
         lines = file.readlines()
-
+    lines = [line.strip() for line in lines]
     return lines
 
 
-TEST_DATA = "dummy"
+TEST_DATA = "dummys"
 if TEST_DATA == "dummy":
     AVAILABLE_PVS = [i for i in range(3)]
 else:
     AVAILABLE_PVS = get_pvs_from_txt("IOC_PVs.txt")
-
 
 @st.experimental_singleton
 def initialize():
     if TEST_DATA == "dummy":
         data_gen = dummy_data_thread.DataGenerator(50)
     else:
-        data_gen = CALiveReader(1, ["kern:mass", "kern:stability"], 10)
+        data_gen = CALiveReader(0.1, AVAILABLE_PVS, 100)
     data_gen.start()
     return data_gen
 
@@ -48,11 +49,17 @@ for i in range(num_plots):
             selected_pvs.append(st.multiselect("Select param", AVAILABLE_PVS, key=i))
         with data_points_col:
             num_data_points.append(
-                st.slider("Number of data points", min_value=100, max_value=1000, key=i)
+                st.slider("Number of data points", min_value=10, max_value=1000, key=i)
             )
-
-        charts.append(st.line_chart(data_gen.df.iloc[:, selected_pvs[i]]))
+        if TEST_DATA == "dummy":
+            charts.append(st.line_chart(data_gen.df.iloc[:, selected_pvs[i]]))
+        else:
+            charts.append(st.line_chart(data_gen.df[selected_pvs[i]]))
 
 for i in range(1000000):
     for idx, chart in enumerate(charts):
-        chart.line_chart(data_gen.df.iloc[:, selected_pvs[idx]])
+
+        if TEST_DATA == "dummy":
+            chart.line_chart(data_gen.df.iloc[:, selected_pvs[idx]])
+        else:
+            chart.line_chart(data_gen.df[selected_pvs[idx]])
