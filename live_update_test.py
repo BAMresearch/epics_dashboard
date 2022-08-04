b8832ce2766037a5f9804f0481f4366d06f80cee
jfrontze <julius.frontzek@bam.de>
2022-08-04 14:50:11 +0200

    Switch between dummy and EPICS data

--- live_update_test.py
+++ live_update_test.py
@@ -1,34 +1,58 @@
+from mimetypes import init
 import streamlit as st
 import pandas as pd
 import numpy as np
 import time
+import dummy_data_thread
+import threading
+from CaprotoReaders.caproto_live_reader import CALiveReader
+from typing import List
 
-df1 = pd.DataFrame(np.random.randn(100, 10), columns=('col %d' % i for i in range(10)))
-df1['index'] = np.arange(100)
-df1.set_index('index', inplace=True)
 
-add = st.button('Add')
-chart = st.line_chart(df1)
+def get_pvs_from_txt(filename: str) -> List[str]:
+    with open(filename) as file:
+        lines = file.readlines()
 
-start = time.time()
-for i in range(30000):
-    df1 = df1.iloc[1:]
-    df2 = pd.DataFrame(np.random.randn(1, 10), columns=('col %d' % e for e in range(10)))
-    df2['index'] = 100+i
-    df2.set_index('index', inplace=True)
-    df1 = pd.concat([df1,df2], )
-    
-    print(i)
-    # Tried to create entirely new line chart every so often to help with performance.
-    # Testing throws an error from time to time because the index i gets reset once in a while and it then seems to run two for loops in "parallel"
-    if i%100 == 0:
-        chart.empty()
-        chart = st.line_chart(df1)
+    return lines
+
+
+TEST_DATA = "dummy"
+if TEST_DATA == "dummy":
+    AVAILABLE_PVS = [i for i in range(3)]
+else:
+    AVAILABLE_PVS = get_pvs_from_txt("IOC_PVs.txt")
+
+
+@st.experimental_singleton
+def initialize():
+    if TEST_DATA == "dummy":
+        data_gen = dummy_data_thread.DataGenerator(50)
     else:
-        chart.line_chart(df1)
-    
-    if i%1000 == 0 and i>0:
-        stop = time.time()
-        duration = stop - start
-        print(f"{duration = }")
-        start = stop
+        data_gen = CALiveReader(1, ["kern:mass", "kern:stability"], 10)
+    data_gen.start()
+    return data_gen
+
+
+data_gen = initialize()
+charts = []
+selected_pvs = []
+num_data_points = []
+
+num_plots = st.slider("Number of plots", min_value=1, max_value=3)
+
+for i in range(num_plots):
+    with st.container():
+        pv_col, data_points_col = st.columns([1, 1])
+
+        with pv_col:
+            selected_pvs.append(st.multiselect("Select param", AVAILABLE_PVS, key=i))
+        with data_points_col:
+            num_data_points.append(
+                st.slider("Number of data points", min_value=100, max_value=1000, key=i)
+            )
+
+        charts.append(st.line_chart(data_gen.df.iloc[:, selected_pvs[i]]))
+
+for i in range(1000000):
+    for idx, chart in enumerate(charts):
+        chart.line_chart(data_gen.df.iloc[:, selected_pvs[idx]])
